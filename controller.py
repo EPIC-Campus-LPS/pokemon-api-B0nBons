d6d139fbb014a1e5e6c31f3584c30859b95d8833
Baani <165845602+B0nBons@users.noreply.github.com>
2025-09-27 19:55:34 -0600

    Update controller.py
    Add better comments

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ class PokemonController:
     def __init__(self, model, view):
         self.model = model
         self.view = view
+        # Initialize variables
 
         self.view.set_next_callback(self.handle_increment)
         self.view.set_prev_callback(self.handle_decrement)
@@ -11,15 +12,15 @@ class PokemonController:
 
     def handle_increment(self):
         self.model.increment()
-        self.update_view() # Update the view as soon as user clicks next or previous
+        self.update_view() # Update the view as soon as user clicks next
 
     def handle_decrement(self):
         self.model.decrement()
-        self.update_view()
+        self.update_view() # Just like the other one, updates the view when user clicks previous
 
     def update_view(self):
         # Get data from model
         name, image = self.model.get_pokemon_data()
         # Update view
         self.view.set_label(f"{self.model.get_value()}: {name}") #Print pokemon number, and the pokemon name
-        self.view.set_image(image)
+        self.view.set_image(image) # Set pokemon sprite
