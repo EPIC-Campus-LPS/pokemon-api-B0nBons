5f3bac1112e23d96f2a068769c28e240459c7c11
Baani <165845602+B0nBons@users.noreply.github.com>
2025-09-27 19:50:51 -0600

    Update controller.py

--- controller.py
+++ controller.py
@@ -2,13 +2,24 @@ class PokemonController:
     def __init__(self, model, view):
         self.model = model
         self.view = view
+
         self.view.set_next_callback(self.handle_increment)
         self.view.set_prev_callback(self.handle_decrement)
 
+        # Initialize display with Bulbasaur (first pokemon)
+        self.update_view()
+
     def handle_increment(self):
         self.model.increment()
-        self.view.set_label(self.model.get_value())
+        self.update_view() # Update the view as soon as user clicks next or previous
 
     def handle_decrement(self):
         self.model.decrement()
-        self.view.set_label(self.model.get_value())
+        self.update_view()
+
+    def update_view(self):
+        # Get data from model
+        name, image = self.model.get_pokemon_data()
+        # Update view
+        self.view.set_label(f"{self.model.get_value()}: {name}") #Print pokemon number, and the pokemon name
+        self.view.set_image(image)
