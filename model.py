b600996e6e851bd191b41a1614a2c9dec67b232f
Baani <165845602+B0nBons@users.noreply.github.com>
2025-09-27 19:57:17 -0600

    Update model.py
    Add comments

--- model.py
+++ model.py
@@ -11,14 +11,16 @@ class PokemonModel:
     def increment(self):
         if self.number < self.max_pokemon:
             self.number += 1
+        # Increases while making sure it stays under the max limit
 
     def decrement(self):
         if self.number > 1:
             self.number -= 1
+        # Decreases while ensuring it stays at a min of 1 (could change to go to end when pressing back at 1)
 
     def get_value(self):
         return self.number
-
+        
     def get_pokemon_data(self):
         # Get the pokemon name and its sprite from poke api
         url = f"https://pokeapi.co/api/v2/pokemon/{self.number}"
@@ -26,6 +28,7 @@ class PokemonModel:
         data = response.json()
         name = data["name"].title()
         sprite_url = data["sprites"]["front_default"]
+        # Get the sprite URL so it can be downloaded to the computer
 
         # Fetch the sprite image
         img_response = requests.get(sprite_url)
