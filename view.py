fe28bb9f2c3581ae6309c312ad60d628d15af302
Baani <165845602+B0nBons@users.noreply.github.com>
2025-09-27 19:52:29 -0600

    Update view.py

--- view.py
+++ view.py
@@ -1,24 +1,35 @@
 import tkinter as tk
+from PIL import ImageTk
 
 class PokemonView(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.label = tk.Label(text="0", font=("Helvetica", 32), master=master)
-        self.label.grid(row=0, column=0)
-        
+        self.name_label = tk.Label(text="", font=("Helvetica", 20), master=master)
+        self.name_label.grid(row=0, column=0)
+
+        # Image label
+        self.image_label = tk.Label(master=master)
+        self.image_label.grid(row=1, column=0)
+
         self.frame = tk.Frame(master=master)
-        self.frame.grid(row=3,column=0)
+        self.frame.grid(row=2, column=0, pady=10)
+
+        # Finally figured out how to add buttons
+        self.prev_button = tk.Button(text="Previous", master=self.frame)
+        self.prev_button.grid(row=0, column=0, padx=5)
 
         self.next_button = tk.Button(text="Next", master=self.frame)
-        self.next_button.grid(row=0, column=1)
-
-        self.prev_button = tk.Button(text="Previous",master=self.frame)
-        self.prev_button.grid(row=0, column=0)
+        self.next_button.grid(row=0, column=1, padx=5)
 
 
     def set_label(self, text):
-        self.label.config(text=str(text))
+        self.name_label.config(text=str(text))
+
+    def set_image(self, pil_image):
+        # set image so that its compatible with ImageTK
+        self._photo_image = ImageTk.PhotoImage(pil_image)
+        self.image_label.config(image=self._photo_image)
 
     def set_next_callback(self, callback):
         self.next_button.config(command=callback)
